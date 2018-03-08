c0c66813c1935dd8bb850468c0a31bfd2f39e00b
louisbookoff <lsbookof@gmail.com>
2018-03-08 16:37:10 -0500

    Authentication: Curl Scripts
    - Sign out curl complete
    - Change password curl complete

---
+++ scripts/auth/change-password.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl "https://salty-everglades-61557.herokuapp.com/change-password/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLDPW}"'",
+      "new": "'"${NEWPW}"'"
+  }
+}'
+
+echo
