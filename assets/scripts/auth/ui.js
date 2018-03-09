5a41eaeb82149fa6e69e8739d05ba709d8ae2c7d
louisbookoff <lsbookof@gmail.com>
2018-03-09 10:20:30 -0500

    Added user interaction message

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,7 +3,7 @@ const store = require('../store')
 
 const signUpSuccess = function (data) {
   // if ($(.form-group).val() === $(.confirmation).val())
-  $('#message').text('Signed up succesfully')
+  $('#message').text('Signed up successful!')
   $('#message').css('background-color', 'green')
   $('.close').click()
   $('#exampleInputEmail1').val('')
